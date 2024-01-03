425a9d500ab99bb458cbf8b60d7f606271692eed
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-01-03 01:53:16 +0100

    Introduced comparator structs

--- src/median_heuristic.hpp
+++ src/median_heuristic.hpp
@@ -6,6 +6,7 @@
 
 #include "bipartite_graph.hpp"
 #include "crossings.hpp"
+#include "debug.hpp"
 #include "random.hpp"
 
 namespace pace2024 {
@@ -35,14 +36,12 @@ inline void fill_with_identity(std::vector<T>& ordering) {
  */
 template <typename T>
 inline T compute_median(const std::vector<T>& adjacency_list) {
-    std::size_t nof_neighbors = adjacency_list.size();
+    const std::size_t nof_neighbors = adjacency_list.size();
     if (nof_neighbors == 0) {
         return 0;
     } else {
-        std::size_t nof_neighbors_2 = nof_neighbors / 2;
-        return nof_neighbors % 2 == 0
-                   ? (adjacency_list[nof_neighbors_2 - 1] + adjacency_list[nof_neighbors_2]) / 2
-                   : adjacency_list[nof_neighbors_2];
+        // todo: median for nof_neighbors even
+        return adjacency_list[nof_neighbors / 2];
     }
 }
 
@@ -55,35 +54,31 @@ inline T compute_median(const std::vector<T>& adjacency_list) {
  * @param medians
  */
 template <typename T>
-inline void compute_medians(const std::vector<std::vector<T>>& adjacency_lists, std::vector<T>& medians) {
-    std::size_t n1 = adjacency_lists.size();
+inline void compute_medians(const general_bipartite_graph<T>& graph, std::vector<T>& medians) {
+    const std::size_t n1 = graph.get_n1();
+    auto adjacency_lists = graph.get_adjacency_lists();
     medians.resize(n1);
     for (std::size_t i = 0; i < n1; ++i) {
-        medians[i] = compute_median(adjacency_lists[i]);
+        const T median = compute_median(adjacency_lists[i]);
+        assert(median < graph.get_n0());
+        medians[i] = median;
     }
 }
 
-/**
- * @brief computes a linear ordering with the median heuristic
- * we brake ties arbitrarily; this can be improved (todo)
- *
- * @tparam T an unsigned integer type
- * @param graph the bipartite input graph
- * @param ordering an empty array serving as output
- */
 template <typename T>
-void median_heuristic(const general_bipartite_graph<T>& graph,
-                      std::vector<T>& ordering) {
-    const T n1 = graph.get_n1();
+struct median_heuristic_comparator {
+    const std::vector<std::vector<T>>& adjacency_lists;
+    const std::vector<T>& medians;
 
-    ordering.resize(n1);
-    fill_with_identity(ordering);
+    median_heuristic_comparator(const std::vector<std::vector<T>>& adjacency_lists,
+                                const std::vector<T>& medians)
+        : adjacency_lists(adjacency_lists),
+          medians(medians) {
+        assert(adjacency_lists.size() == medians.size());
+    }
 
-    std::vector<T> medians(n1);
-    const std::vector<std::vector<T>>& adjacency_lists = graph.get_adjacency_lists();
-    compute_medians(adjacency_lists, medians);
-
-    sort(ordering.begin(), ordering.end(), [&](const T& a, const T& b) -> bool {
+    bool operator()(const T& a, const T& b) {
+        assert(a < adjacency_lists.size() && b < adjacency_lists.size());
         if (medians[a] < medians[b]) {
             return true;
         } else if (medians[a] > medians[b]) {
@@ -95,7 +90,30 @@ void median_heuristic(const general_bipartite_graph<T>& graph,
         } else {
             return coinflip();
         }
-    });
+    }
+};
+
+/**
+ * @brief computes a linear ordering with the median heuristic
+ *
+ * @tparam T an unsigned integer type
+ * @param graph the bipartite input graph
+ * @param ordering an empty array serving as output
+ */
+template <typename T>
+void median_heuristic(const general_bipartite_graph<T>& graph,
+                      std::vector<T>& ordering) {
+    const std::size_t n1 = graph.get_n1();
+
+    ordering.resize(n1);
+    fill_with_identity(ordering);
+
+    std::vector<T> medians(n1);
+    const std::vector<std::vector<T>>& adjacency_lists = graph.get_adjacency_lists();
+    compute_medians(graph, medians);
+
+    median_heuristic_comparator<T> comparator(adjacency_lists, medians);
+    sort(ordering.begin(), ordering.end(), comparator);
 }
 
 /**
@@ -117,6 +135,41 @@ inline T compute_random_median(const std::vector<T>& adjacency_list, std::unifor
         return adjacency_list[(std::size_t)(distribution(generator) * nof_neighbors)];
     }
 }
+
+template <typename T>
+struct prob_median_heuristic_comparator {
+    const std::vector<std::vector<T>>& adjacency_lists;
+    const std::vector<T>& random_medians;
+    const std::vector<T>& medians;
+
+    prob_median_heuristic_comparator(const std::vector<std::vector<T>>& adjacency_lists,
+                                     const std::vector<T>& random_medians,
+                                     const std::vector<T>& medians)
+        : adjacency_lists(adjacency_lists),
+          random_medians(random_medians),
+          medians(medians) {
+        assert(adjacency_lists.size() == random_medians.size() && random_medians.size() == medians.size());
+    }
+
+    bool operator()(const T& a, const T& b) {
+        assert(a < medians.size() && b < medians.size());
+        if (random_medians[a] < random_medians[b]) {
+            return true;
+        } else if (random_medians[a] > random_medians[b]) {
+            return false;
+        } else if (medians[a] < medians[b]) {  // from here random_medians[a] == random_medians[b]
+            return true;
+        } else if (medians[a] > medians[b]) {  // from here medians[a] == medians[b]
+            return false;
+        } else if (adjacency_lists[a].size() % 2 == 1) {
+            return true;
+        } else if (adjacency_lists[b].size() % 2 == 1) {
+            return false;
+        } else {
+            return coinflip();
+        }
+    }
+};
 
 /**
  * @brief first calls median_heuristic for an initial solution
@@ -149,29 +202,13 @@ T prob_median_heuristic(const general_bipartite_graph<T>& graph,
     auto adjacency_lists = graph.get_adjacency_lists();
 
     // run the probabilistic algorithm to find a better solution
+    prob_median_heuristic_comparator<T> comparator(adjacency_lists, random_medians, medians);
     for (std::size_t iteration = 0; iteration < nof_iterations; ++iteration) {
         for (std::size_t i = 0; i < n1; ++i) {
             random_medians[i] = compute_random_median(adjacency_lists[i], distribution);
         }
 
-        sort(another_ordering.begin(), another_ordering.end(),
-             [&](const T& a, const T& b) -> bool {
-                 if (random_medians[a] < random_medians[b]) {
-                     return true;
-                 } else if (random_medians[a] > random_medians[b]) {
-                     return false;
-                 } else if (medians[a] < medians[b]) {  // from here random_medians[a] == random_medians[b]
-                     return true;
-                 } else if (medians[a] > medians[b]) {  // from here medians[a] == medians[b]
-                     return false;
-                 } else if (adjacency_lists[a].size() % 2 == 1) {
-                     return true;
-                 } else if (adjacency_lists[b].size() % 2 == 1) {
-                     return false;
-                 } else {
-                     return coinflip();
-                 }
-             });
+        sort(another_ordering.begin(), another_ordering.end(), comparator);
 
         T candidate = compute_crossings(cr_matrix, another_ordering);
         if (candidate < best) {
