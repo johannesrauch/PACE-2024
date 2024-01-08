a137539449810ef9c95613de6fa4f034878c5857
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-01-08 14:15:31 +0100

    All tiny_test_set tests

--- test/branch_and_cut.cpp
+++ test/branch_and_cut.cpp
@@ -1,7 +1,33 @@
 #include "branch_and_cut.hpp"
 
+#include <string>
+
 #include "crossings.hpp"
 #include "output.hpp"
+
+void tiny_test_set() {
+    const std::string tiny_tests[13] =
+        {"complete_4_5.gr",
+         "cycle_8_shuffled.gr",
+         "cycle_8_sorted.gr",
+         "grid_9_shuffled.gr",
+         "ladder_4_4_shuffled.gr",
+         "ladder_4_4_sorted.gr",
+         "matching_4_4.gr",
+         "path_9_shuffled.gr",
+         "path_9_sorted.gr",
+         "plane_5_6.gr",
+         "star_6.gr",
+         "tree_6_10.gr",
+         "website_20.gr"};
+    const uint16_t solutions[13] = {60, 4, 3, 17, 11, 3, 0, 6, 0, 0, 0, 13, 17};
+    for (uint16_t i = 0; i < 13; ++i) {
+        pace2024::uint16_instance instance("tiny_test_set/" + tiny_tests[i]);
+        pace2024::branch_and_cut<uint16_t> solver(instance);
+        solver.solve(false);
+        assert(solver.get_nof_crossings() == solutions[i]);
+    }
+}
 
 int main() {
     {
@@ -32,6 +58,7 @@ int main() {
                pace2024::nof_crossings(solver.get_crossing_matrix(), solver.get_ordering()));
     }
 
+    tiny_test_set();
     std::cout << "TEST::PACE2024::BRANCH_AND_CUT: OKAY" << std::endl;
     return 0;
 }
