8702d546b08752b69234078702d4974333237f3c
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-02-27 16:47:57 +0100

    refactored tests; docs

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -1,35 +1,36 @@
-#include <iostream>
-
 #include "matrix.hpp"
 
+#include <iostream>
+#include <filesystem>
+
+/**
+ * @brief tests the different matrices and the different
+ * ways to compute the crossing number matrix.
+ * 
+ * @tparam T vertex type
+ * @param graph 
+ */
+template<typename T>
+void test_matrix_and_compute_crossing_numbers(const pace2024::general_bipartite_graph<T>& graph) {
+    const std::size_t n1{graph.get_n1()};
+    pace2024::uint64_folded_matrix ref_matrix(n1);
+    pace2024::uint64_matrix tst_matrix_1(graph);
+    pace2024::uint64_folded_matrix tst_matrix_2(graph);
+    pace2024::test::compute_crossing_numbers_naivly(graph, ref_matrix);
+    
+    assert(pace2024::test::equals(ref_matrix, tst_matrix_1, true));
+    assert(pace2024::test::equals(ref_matrix, tst_matrix_2, true));
+}
+
+/**
+ * @brief tests matrix.hpp; its classes and functions
+ * 
+ * @return int 
+ */
 int main() {
-    pace2024::uint64_instance instance("tiny_test_set/tree_6_10.gr");
-    pace2024::uint64_bipartite_graph graph(instance);
-
-    uint64_t n1 = graph.get_n1();
-    pace2024::folded_matrix<uint64_t> ref_cr_matrix(n1);
-    pace2024::compute_crossing_numbers_naivly(graph, ref_cr_matrix);
-    pace2024::matrix<uint64_t> cr_matrix(graph);
-
-    // pace2024::print_matrix(ref_cr_matrix);
-    // std::cout << std::endl;
-    // pace2024::print_matrix(cr_matrix);
-
-    for (std::size_t i = 0; i < n1; ++i) {
-        for (std::size_t j = 0; j < n1; ++j) {
-            if (i == j) continue;
-            assert(ref_cr_matrix(i, j) == cr_matrix(i, j));
-        }
-    }
-
-    pace2024::folded_matrix<uint64_t> fl_matrix(graph);
-    for (uint64_t i = 0; i < n1; ++i) {
-        for (uint64_t j = 0; j < n1; ++j) {
-            if (i == j) continue;
-            // std::cout << ref_cr_matrix(i, j) << "," << fl_matrix(i, j)
-            //         << std::endl;
-            assert(ref_cr_matrix(i, j) == fl_matrix(i, j));
-        }
+    for (const auto& file : std::filesystem::directory_iterator("tiny_test_set")) {
+        pace2024::uint16_bipartite_graph graph(static_cast<const std::string>(file.path()));
+        test_matrix_and_compute_crossing_numbers(graph);        
     }
 
     std::cout << "TEST::PACE2024::CROSSING_NUMBER_MATRIX: OKAY" << std::endl;
