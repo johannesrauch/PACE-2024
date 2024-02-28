896944b0ffb49d471b398b4c95d9b11d1061ec2b
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-02-28 09:14:49 +0100

    docs

--- src/topological_sort.hpp
+++ src/topological_sort.hpp
@@ -9,6 +9,18 @@
 
 namespace pace2024 {
 
+/**
+ * @brief a depth-first search through the given graph.
+ * used by topological_sort below.
+ * 
+ * @tparam T vertex type
+ * @param graph 
+ * @param ordering vector, where is stored
+ * @param visited 
+ * @param v current vertex
+ * @return true if a topological sort was found
+ * @return false if a (directed) cycle was found
+ */
 template <typename T>
 bool topological_sort_dfs(const general_graph<T>& graph,
                           std::vector<T>& ordering,       //
@@ -29,6 +41,16 @@ bool topological_sort_dfs(const general_graph<T>& graph,
     return true;
 }
 
+/**
+ * @brief computes a topological sort (if possible) of the given graph
+ * and stores it in the vector ordering.
+ * 
+ * @tparam T vertex type
+ * @param graph 
+ * @param ordering 
+ * @return true if a topological sort was found
+ * @return false if a (directed) cycle was found
+ */
 template <typename T>
 bool topological_sort(const general_graph<T>& graph, std::vector<T>& ordering) {
     const T n = graph.get_n();
