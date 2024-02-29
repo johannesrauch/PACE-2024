7db220b520e0ea171b91323cd0dde21b7edb6660
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-02-29 17:04:01 +0100

    test for medium sized instances

---
+++ test/branch_and_cut_medium.cpp
@@ -0,0 +1,57 @@
+#include <filesystem>
+#include <fstream>
+#include <set>
+#include <string>
+
+#include "branch_and_cut.hpp"
+#include "crossing_number.hpp"
+#include "output.hpp"
+#include "printf.hpp"
+#include "test_utils.hpp"
+
+void test_solver_w_instance(std::filesystem::path filepath_instance) {
+    fmt::printf("%s\n", static_cast<std::string>(filepath_instance));
+    pace2024::uint16_bipartite_graph graph(filepath_instance);
+    pace2024::branch_and_cut<uint16_t, uint32_t> solver(graph);
+    solver.solve(false);
+
+    uint32_t ref_nof_crossings =
+        pace2024::test::get_ref_nof_crossings<uint32_t>(filepath_instance);
+
+    assert(ref_nof_crossings == solver.get_nof_crossings());
+    // fmt::printf("%s,%s\n", solver.get_nof_crossings(), solutions[i]);
+    uint32_t test_nof_crossings =
+        pace2024::crossing_number_of<uint16_t, uint32_t>(graph, solver.get_ordering());
+    (void)test_nof_crossings;
+    assert(ref_nof_crossings == test_nof_crossings);
+}
+
+/**
+ * @brief test branch_and_cut solver on medium_test_set
+ */
+void test_solver_w_medium_test_set() {
+    // sort first
+    std::set<std::filesystem::path> sorted_directory;
+    for (const auto& file : std::filesystem::directory_iterator("medium_test_set")) {
+        if (!file.is_regular_file()) continue;
+        sorted_directory.insert(file.path());
+    }
+
+    // execute all testcases
+    for (const auto& filepath_instance : sorted_directory) {
+        test_solver_w_instance(filepath_instance);
+    }
+}
+
+/**
+ * @brief tests branch_and_cut on medium_test_set
+ *
+ * @return int
+ */
+int main() {
+    test_solver_w_medium_test_set();
+    // test_solver_w_instance("medium_test_set/13.gr");
+
+    std::cout << "TEST::PACE2024::BRANCH_AND_CUT_MEDIUM:\t\tOK" << std::endl;
+    return 0;
+}
---
+++ test/test_utils.hpp
@@ -0,0 +1,28 @@
+#ifndef PACE2024_TEST_UTILS_HPP
+#define PACE2024_TEST_UTILS_HPP
+
+#include <filesystem>
+
+namespace pace2024 {
+
+namespace test {
+
+template <typename R>
+R get_ref_nof_crossings(std::filesystem::path filepath_instance) {
+    auto filepath_nof_crossings =
+        filepath_instance.parent_path() / "nof_crossings" / filepath_instance.filename();
+    filepath_nof_crossings.replace_extension(".txt");
+    // fmt::printf("%s\n", static_cast<std::string>(filepath_nof_crossings));
+    std::ifstream file_nof_crossings(filepath_nof_crossings);
+    assert(file_nof_crossings.good());
+    uint32_t ref_nof_crossings;
+    file_nof_crossings >> ref_nof_crossings;
+    file_nof_crossings.close();
+    return ref_nof_crossings;
+}
+
+};  // namespace test
+
+};  // namespace pace2024
+
+#endif
