89aa3709c45995e6ffdf4edf1954461dff222ac2
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-01 16:35:38 +0100

    flush after print

--- src/debug_printf.hpp
+++ src/debug_printf.hpp
@@ -1,16 +1,19 @@
 #ifndef PACE2024_DEBUG_PRINTF_HPP
 #define PACE2024_DEBUG_PRINTF_HPP
 
+#include <iostream>
+
 #include "printf.hpp"
 
 #ifdef NDEBUG_PRINT
 #define PACE2024_DEBUG_PRINTF(...) \
-    do {                            \
-        fmt::printf(__VA_ARGS__);          \
+    do {                           \
+        fmt::printf(__VA_ARGS__);  \
+        std::cout << std::flush;   \
     } while (false)
 #else
 #define PACE2024_DEBUG_PRINTF(...) \
-    do {                            \
+    do {                           \
     } while (false)
 #endif
 
