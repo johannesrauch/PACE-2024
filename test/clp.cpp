4889ff1136b0bc5bdc0b8ad8589757e78eb5417e
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-11 07:51:58 +0100

    highs compilation test

--- test/clp.cpp
+++
@@ -1,8 +0,0 @@
-#include "coin-or/ClpSimplex.hpp"
-
-int main() {
-    ClpSimplex model;
-    model.resize(42, 42);
-    std::cout << "TEST::PACE2024::CLP:\t\t\t\tOK" << std::endl;
-    return 0;
-}
---
+++ test/highs.cpp
@@ -0,0 +1,8 @@
+#include <highs/Highs.h>
+#include <iostream>
+
+int main() {
+    Highs lp;
+    std::cout << "TEST::PACE2024::HiGHS:\t\t\t\tOK" << std::endl;
+    return 0;
+}
