672681453163875bce4344c7a417b6010fe83145
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-15 08:58:13 +0100

    #include <iostream>

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -3,6 +3,7 @@
 
 #include <ctime>
 #include <filesystem>
+#include <iostream>
 
 #include "printf.hpp"
 
@@ -23,9 +24,9 @@ double time_in_ms(const std::clock_t start, const std::clock_t end) {
 
 template <typename R = uint32_t>
 R get_ref_nof_crossings(std::filesystem::path filepath_instance) {
-
-    auto filepath_nof_crossings =
-        filepath_instance.parent_path().parent_path() / "nof_crossings" / filepath_instance.filename();
+    auto filepath_nof_crossings = filepath_instance.parent_path().parent_path() /  //
+                                  "nof_crossings" /                                //
+                                  filepath_instance.filename();
     filepath_nof_crossings.replace_extension(".txt");
     // fmt::printf("%s\n", static_cast<std::string>(filepath_nof_crossings));
     std::ifstream file_nof_crossings(filepath_nof_crossings);
