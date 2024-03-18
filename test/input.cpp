02e42b37724ab5bdc3f82f994639fa59a0f4d567
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-18 13:31:15 +0100

    input test

--- test/input.cpp
+++ test/input.cpp
@@ -1,14 +1,15 @@
 #include "input.hpp"
+#include "test_utils.hpp"
 
 #include <cassert>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-std::uint64_t ref_n_fixed = 2;
-std::uint64_t ref_n_free = 6;
-std::uint64_t ref_m = 6;
-std::vector<std::pair<uint64_t, uint64_t>> ref_edges = {{1, 3}, {2, 4}, {1, 5},
+std::uint16_t ref_n_fixed = 2;
+std::uint16_t ref_n_free = 6;
+std::uint16_t ref_m = 6;
+std::vector<std::pair<uint16_t, uint16_t>> ref_edges = {{1, 3}, {2, 4}, {1, 5},
                                                         {2, 6}, {1, 7}, {2, 8}};
 
 /**
@@ -17,15 +18,17 @@ std::vector<std::pair<uint64_t, uint64_t>> ref_edges = {{1, 3}, {2, 4}, {1, 5},
  * @return int 
  */
 int main() {
-    pace2024::uint64_input input("tiny_test_set/star_6.gr");
+    const std::filesystem::path filepath{"tiny_test_set/instances/star_6.gr"};
+    pace2024::bipartite_graph graph;
+    pace2024::parse_input(filepath, graph);
 
-    assert(ref_n_fixed == input.get_n0());
-    assert(ref_n_free == input.get_n1());
-    assert(ref_m == input.get_m());
-    auto edges = input.get_edges();
+    assert(ref_n_fixed == graph.get_n_fixed());
+    assert(ref_n_free == graph.get_n_free());
+    assert(ref_m == graph.get_m());
+    const auto& edges = graph.get_edges();
     for (uint64_t i = 0; i < ref_m; ++i) {
-        assert(ref_edges[i].first == edges[i].first);
-        assert(ref_edges[i].second == edges[i].second);
+        assert(ref_edges[i].first == edges[i].first + 1);
+        assert(ref_edges[i].second == edges[i].second + 1 + ref_n_fixed);
     }
 
     std::cout << "TEST::PACE2024::INPUT:\t\t\t\tOK" << std::endl;
