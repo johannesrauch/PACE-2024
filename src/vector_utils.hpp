8768db63dd5a810eb8553136512c4500513cc636
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-18 17:35:39 +0100

    union method

--- src/vector_utils.hpp
+++ src/vector_utils.hpp
@@ -8,15 +8,15 @@ namespace pace2024 {
 
 /**
  * @brief returns the number of common elements in the ascending ordered vectors
- * 
+ *
  * @tparam T element type of vectors
  * @tparam R return type
- * @param vec1 
- * @param vec2 
+ * @param vec1
+ * @param vec2
  * @return R number of common elements
  */
 template <typename T, typename R = std::size_t>
-R sorted_vector_intersection(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+R sorted_vector_intersection(const std::vector<T> &vec1, const std::vector<T> &vec2) {
     R size_of_intersection{0};
     for (std::size_t i = 0, j = 0; i < vec1.size() && j < vec2.size();) {
         if (vec1[i] == vec2[j]) {
@@ -32,6 +32,35 @@ R sorted_vector_intersection(const std::vector<T>& vec1, const std::vector<T>& v
     return size_of_intersection;
 }
 
-};
+/**
+ * @brief returns the union of the ascending sorted vectors `vec1` and `vec2` in `out`
+ * 
+ * @tparam T element type of vectors
+ * @param vec1 
+ * @param vec2 
+ * @param out out-parameter, union of `vec1` and `vec2` is stored here
+ */
+template <typename T>
+void sorted_vector_union(const std::vector<T> &vec1, const std::vector<T> &vec2, std::vector<T> &out) {
+    out.clear();
+    out.reserve(vec1.size() + vec2.size());
+    std::size_t i = 0, j = 0;
+    std::size_t len1 = vec1.size(), len2 = vec2.size();
+    while (i < len1 && j < len2) {
+        if (vec1[i] == vec2[j]) {
+            out.emplace_back(vec1[i]);
+            ++i;
+            ++j;
+        } else if (vec1[i] < vec2[j]) {
+            out.emplace_back(vec1[i]);
+            ++i;
+        } else {
+            out.emplace_back(vec2[j]);
+            ++j;
+        }
+    }
+}
+
+};  // namespace pace2024
 
 #endif
