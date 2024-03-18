cb11c01342763bbd1814070f7671d1c4d42a04b9
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-18 13:35:02 +0100

    highs wrapper test

--- test/lp_wrapper.cpp
+++ test/lp_wrapper.cpp
@@ -1,8 +1,14 @@
+#include <filesystem>
+#include <iostream>
+
+#include "bipartite_graph.hpp"
+#include "input.hpp"
 #include "lp_wrapper.hpp"
 
 int main() {
-    const std::string filepath = "tiny_test_set/cycle_8_sorted.gr";
-    pace2024::uint16_bipartite_graph graph(filepath);
-    pace2024::clp_wrapper lp(graph);
+    const std::filesystem::path filepath = "tiny_test_set/instances/cycle_8_sorted.gr";
+    pace2024::bipartite_graph graph;
+    pace2024::parse_input(filepath, graph);
+    pace2024::highs_wrapper lp(graph);
     std::cout << "TEST::PACE2024::LP_WRAPPER:\t\t\t\tOK" << std::endl;
 }
