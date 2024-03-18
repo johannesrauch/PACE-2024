7a7bbd706dfac131c43b7d90f168cb804c157b7f
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-18 17:35:45 +0100

    start patterns

---
+++ src/patterns.hpp
@@ -0,0 +1,14 @@
+#ifndef PACE2024_PATTERNS_HPP
+#define PACE2024_PATTERNS_HPP
+
+#include "bipartite_graph.hpp"
+
+namespace pace2024 {
+
+template <typename T>
+void analyze_patterns(const bipartite_graph<T>& graph, const T u, const T v) {
+}
+
+};  // namespace pace2024
+
+#endif
