6e739a63427b3077091f307f5a2de85381316405
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-20 15:34:26 +0100

    get_ref_ordering

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -3,7 +3,9 @@
 
 #include <ctime>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "printf.hpp"
 
@@ -12,21 +14,22 @@ namespace pace2024 {
 namespace test {
 
 /// @brief prints a line :o
-void print_line(std::size_t length) {
+inline void print_line(std::size_t length) {
     for (std::size_t i = 0; i < length; ++i) fmt::printf("-");
     fmt::printf("\n");
 }
 
 /// @brief returns the elapsed time between `start` and `end` in ms
-double time_in_ms(const std::clock_t start, const std::clock_t end) {
+inline double time_in_ms(const std::clock_t start, const std::clock_t end) {
     return 1000.0 * (end - start) / CLOCKS_PER_SEC;
 }
 
 template <typename R = uint32_t>
 R get_ref_nof_crossings(std::filesystem::path filepath_instance) {
-    auto filepath_nof_crossings = filepath_instance.parent_path().parent_path() /  //
-                                  "nof_crossings" /                                //
-                                  filepath_instance.filename();
+    auto filepath_nof_crossings =                        //
+        filepath_instance.parent_path().parent_path() /  //
+        "nof_crossings" /                                //
+        filepath_instance.filename();
     filepath_nof_crossings.replace_extension(".txt");
     // fmt::printf("%s\n", static_cast<std::string>(filepath_nof_crossings));
     std::ifstream file_nof_crossings(filepath_nof_crossings);
@@ -35,6 +38,31 @@ R get_ref_nof_crossings(std::filesystem::path filepath_instance) {
     file_nof_crossings >> ref_nof_crossings;
     file_nof_crossings.close();
     return ref_nof_crossings;
+}
+
+template <typename T>
+void get_ref_ordering(const std::filesystem::path filepath_instance,  //
+                      const std::size_t n_fixed,                      //
+                      const std::size_t n_free,                       //
+                      std::vector<T> &ordering) {
+    auto filepath_solution =                           //
+        filepath_instance.parent_path().parent_path()  //
+        / "solutions"                                  //
+        / filepath_instance.filename();
+    filepath_solution.replace_extension(".sol");
+    std::ifstream file_solution(filepath_solution);
+    assert(file_solution.good());
+
+    ordering.clear();
+    ordering.reserve(n_free);
+    for (std::size_t i = 0; i < n_free; ++i) {
+        T v;
+        file_solution >> v;
+        assert(v >= n_fixed + 1);
+        v -= n_fixed + 1;
+        assert(v < n_free);
+        ordering.emplace_back(v);
+    }
 }
 
 };  // namespace test
