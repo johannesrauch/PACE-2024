70455701f2be4998a5378a4e6f1b042235612f27
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-21 16:18:56 +0100

    renamed patterns to oracle

---
+++ src/oracle.hpp
@@ -0,0 +1,141 @@
+#ifndef PACE_ORACLE_HPP
+#define PACE_ORACLE_HPP
+
+#include "instance.hpp"
+#include "crossings.hpp"
+#include "vector_utils.hpp"
+
+namespace pace {
+
+enum pattern { indeterminate = 0,
+               u_before_v,
+               v_before_u };
+
+/**
+ * @brief oracle that tells you if we are able to fix u < v or v < u
+ *
+ * @tparam T vertex type
+ * @tparam R crossing number type
+ */
+template <typename T, typename R>
+class position_oracle {
+    /// @brief instance graph
+    const bipartite_graph<T> &graph;
+
+    /// @brief crossing number matrix
+    const folded_matrix<R> &cr_matrix;
+
+    /// @brief lower and upper bound on the optimal value
+    uint32_t lower_bound, upper_bound{0};
+
+   public:
+    /// @brief initializes the oracle
+    position_oracle(const instance<T, R> &instance,
+                    const uint32_t lower_bound,
+                    const uint32_t upper_bound)
+        : graph(instance.graph()),  //
+          cr_matrix(instance.cr_matrix()),
+          lower_bound(lower_bound),
+          upper_bound(upper_bound) {
+        assert(lower_bound <= upper_bound);
+    }
+
+    /// @brief tells you if we are able to fix u < v or v < u
+    pattern foresee(const T u, const T v) {
+        const R &c_uv = cr_matrix(u, v);
+        const R &c_vu = cr_matrix(v, u);
+        if (c_uv == c_vu) return indeterminate;
+
+        // c_uv != c_vu from her
+        pattern p = based_on_crossing_numbers(c_uv, c_vu);
+        if (p != indeterminate) return p;
+
+        p = based_on_bounds(c_uv, c_vu);
+        if (p != indeterminate) return p;
+
+        p = based_on_pattern(u, v, c_uv, c_vu);
+        if (p != indeterminate) return p;
+
+        return indeterminate;
+    }
+
+    /**
+     * @brief assumption: c_uv != c_vu.
+     * - if c_uv == 0, we can fix u < v, and
+     * - if c_vu == 0, we can fix v < u.
+     */
+    inline pattern based_on_crossing_numbers(const R &c_uv, const R &c_vu) {
+        assert(c_uv != c_vu);
+        if (c_uv == 0) return u_before_v;
+        if (c_vu == 0) return v_before_u;
+        return indeterminate;
+    }
+
+    /**
+     * @brief if it were the other way around, we already would have more than upper_bound crossings
+     */
+    inline pattern based_on_bounds(const R &c_uv, const R &c_vu) {
+        const uint32_t diff = upper_bound - lower_bound;
+        if (c_uv > c_vu && c_uv - c_vu > diff) {
+            return v_before_u;
+        }
+        if (c_vu > c_uv && c_vu - c_uv > diff) {
+            return u_before_v;
+        }
+        return indeterminate;
+    }
+
+    /**
+     * @brief based on an idea of Dujmovic et al, see https://doi.org/10.1016/j.jda.2006.12.008
+     */
+    inline pattern based_on_pattern(const T &u, const T &v, const R &c_uv, const R &c_vu) {
+        const std::vector<T> &nbors_u = graph.get_neighbors_of_free(u);
+        const std::vector<T> &nbors_v = graph.get_neighbors_of_free(v);
+        // necessary condition for further statements
+        if (nbors_u.size() != nbors_v.size()) return indeterminate;
+        assert(nbors_u.size() > 0 && nbors_v.size() > 0);
+
+        std::vector<T> nbors_uv;
+        sorted_vector_union(nbors_u, nbors_v, nbors_uv);
+        std::size_t i_u = 0, i_v = 0;
+        R a = nbors_u.size(), b = nbors_v.size();
+        for (std::size_t i = 0; i < nbors_uv.size(); ++i) {
+            if (i_u < nbors_u.size() && nbors_uv[i] == nbors_u[i_u]) {
+                assert(a > 0);
+                --a;
+            }
+            if (i_v < nbors_v.size() && nbors_uv[i] == nbors_v[i_v]) {
+                assert(b > 0);
+                --b;
+            }
+
+            if (c_uv < c_vu && a > b) return indeterminate;
+            if (c_uv > c_vu && a < b) return indeterminate;
+
+            if (i_v < nbors_v.size() && nbors_uv[i] == nbors_v[i_v]) {
+                ++a;
+                ++i_v;
+            }
+            if (i_u < nbors_u.size() && nbors_uv[i] == nbors_u[i_u]) {
+                ++b;
+                ++i_u;
+            }
+
+            if (c_uv < c_vu && a > b) return indeterminate;
+            if (c_uv > c_vu && a < b) return indeterminate;
+        }
+        assert(i_u == nbors_u.size());
+        assert(i_v == nbors_v.size());
+        assert(a == nbors_v.size());
+        assert(b == nbors_u.size());
+
+        if (c_uv < c_vu)
+            return u_before_v;
+        else
+            return v_before_u;
+    }
+};
+
+};  // namespace pace
+
+#endif
---
+++ test/oracle.cpp
@@ -0,0 +1,114 @@
+#include "oracle.hpp"
+
+#include <cassert>
+
+#include "crossings.hpp"
+#include "heuristics.hpp"
+#include "instance.hpp"
+#include "test_utils.hpp"
+#include "vector_utils.hpp"
+
+namespace fs = std::filesystem;
+
+template <typename T>
+void insert_based_on_pattern(
+    std::vector<std::pair<T, T>>& vec,
+    const T& u,
+    const T& v,
+    pace::pattern& p) {
+    assert(p != pace::indeterminate);
+    if (p == pace::u_before_v) {
+        vec.emplace_back(u, v);
+    } else {
+        vec.emplace_back(v, u);
+    }
+}
+
+template <typename T, typename R>
+void test_pattern_analyzer(const pace::instance<T, R>& instance, const bool do_test) {
+    const pace::bipartite_graph<T>& graph(instance.graph());
+    const pace::folded_matrix<R>& cr_matrix(instance.cr_matrix());
+    std::vector<T> ordering;
+    std::vector<T> positions;
+    const uint32_t lb = pace::get_lower_bound(cr_matrix);
+    const uint32_t ub = pace::heuristics(instance, ordering);
+    assert(lb <= ub);
+    pace::position_oracle oracle(instance, lb, ub);
+
+    std::vector<std::pair<T, T>> new_fixings;
+    uint32_t c = 0, b = 0, p = 0;
+    const std::size_t n = graph.get_n_free();
+    for (T u = 0; u < n; ++u) {
+        for (T v = u + 1; v < n; ++v) {
+            const R& c_uv = cr_matrix(u, v);
+            const R& c_vu = cr_matrix(v, u);
+            if (c_uv == c_vu) continue;
+
+            // c_uv != c_vu from her
+            pace::pattern pattern = oracle.based_on_crossing_numbers(c_uv, c_vu);
+            if (pattern != pace::indeterminate) {
+                ++c;
+                insert_based_on_pattern(new_fixings, u, v, pattern);
+                continue;
+            }
+
+            pattern = oracle.based_on_bounds(c_uv, c_vu);
+            if (pattern != pace::indeterminate) {
+                ++b;
+                insert_based_on_pattern(new_fixings, u, v, pattern);
+                continue;
+            }
+
+            pattern = oracle.based_on_pattern(u, v, c_uv, c_vu);
+            if (pattern != pace::indeterminate) {
+                ++p;
+                insert_based_on_pattern(new_fixings, u, v, pattern);
+                continue;
+            }
+        }
+    }
+
+    if (do_test) {
+        pace::test::get_ref_ordering(  //
+            instance.filepath, graph.get_n_fixed(), graph.get_n_free(), ordering);
+        pace::inverse(ordering, positions);
+        for (const auto& [u, v] : new_fixings) {
+            assert(positions[u] < positions[v]);
+        }
+    }
+
+    std::size_t n_choose_2 = n * (n - 1) / 2;
+    fmt::printf("%11s|%11u%11u%11.1f|%11u%11u%11u|%11u%11u\n",
+                instance.filepath.filename(),
+                n_choose_2, new_fixings.size(), static_cast<double>(new_fixings.size()) / n_choose_2 * 100,
+                c, b, p, lb, ub);
+    std::cout << std::flush;
+}
+
+void test_pattern_analyzer_with(const fs::path dirpath, const bool do_test) {
+    fmt::printf("start test_pattern_analyzer_with(%s)\n\n", dirpath);
+    fmt::printf("%11s|%11s%11s%11s|%11s%11s%11s|%11s%11s\n",
+                "instance",
+                "vars", "fixed", "%%",
+                "crossing n", "bounds", "pattern",
+                "lb", "ub");
+    pace::test::print_line(103);
+    for (const auto& file : fs::directory_iterator(dirpath)) {
+        pace::instance instance(file.path());
+        test_pattern_analyzer(instance, do_test);
+    }
+    fmt::printf("\nend test_pattern_analyzer_with(%s)\n\n", dirpath);
+}
+
+/**
+ * @brief tests oracle.hpp
+ */
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        test_pattern_analyzer_with("medium_test_set/instances", true);
+    } else {
+        test_pattern_analyzer_with(std::string{argv[1]} + "/instances", false);
+    }
+    std::cout << "TEST::PACE::PATTERNS:\t\t\t\t\tOK" << std::endl;
+    return 0;
+}
