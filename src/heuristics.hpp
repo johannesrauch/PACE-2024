b82990eaa4e3e584aca3a30e7ff9b649437cf4a1
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-22 09:50:30 +0100

    introduced lower bound to identify (some) optimal solutions and avoid exec of probmedian h

--- src/heuristics.hpp
+++ src/heuristics.hpp
@@ -1,6 +1,7 @@
 #ifndef PACE_HEURISTICS_HPP
 #define PACE_HEURISTICS_HPP
 
+#include "crossings.hpp"
 #include "median_heuristic.hpp"
 #include "barycenter_heuristic.hpp"
 #include "shift_heuristic.hpp"
@@ -10,15 +11,23 @@ namespace pace {
 
 template <typename T, typename R>
 uint32_t heuristics(const instance<T, R> &instance, std::vector<T> &ordering) {
-    uint32_t ub = probmedian_heuristic{instance}(ordering);
+    return heuristics(instance, ordering, get_lower_bound(instance.cr_matrix()));
+}
+
+template <typename T, typename R>
+uint32_t heuristics(const instance<T, R> &instance, std::vector<T> &ordering, const uint32_t lb) {
+    uint32_t ub = barycenter_heuristic{instance}(ordering);
+    assert(lb <= ub);
+    if (lb == ub) return ub;
 
     std::vector<T> ordering_(ordering.size());
-    uint32_t ub_ = barycenter_heuristic{instance}(ordering_);
+    uint32_t ub_ = probmedian_heuristic{instance}(ordering_);
     if (ub_ < ub) {
         ub = ub_;
         ordering = ordering_;
     }
 
+    assert(lb <= ub);
     return ub;
 }
 
