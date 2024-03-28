ef2475cb99aa78933d67b74e20fa91c89e7295bc
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-03-28 10:00:47 +0100

    test based_on_degree

--- test/oracle.cpp
+++ test/oracle.cpp
@@ -13,10 +13,10 @@ namespace fs = std::filesystem;
 
 template <typename T>
 inline void insert_based_on_pattern(std::vector<std::pair<T, T>>& vec,  //
-                             pace::digraph<T>& graph,            //
-                             const T& u,                         //
-                             const T& v,                         //
-                             pace::pattern& p) {
+                                    pace::digraph<T>& graph,            //
+                                    const T& u,                         //
+                                    const T& v,                         //
+                                    pace::pattern& p) {
     assert(p != pace::indeterminate);
     if (p == pace::u_before_v) {
         vec.emplace_back(u, v);
@@ -31,48 +31,56 @@ template <typename T, typename R>
 void test_position_oracle(const pace::instance<T, R>& instance, const bool do_test) {
     const pace::bipartite_graph<T>& graph(instance.graph());
     const pace::folded_matrix<R>& cr_matrix(instance.cr_matrix());
+
+    const std::size_t n = graph.get_n_free();
     std::vector<T> ordering;
     const uint32_t ub = pace::heuristics(instance, ordering);
-    pace::digraph digraph(graph.get_n_free());
+    pace::digraph restriction_graph(n);
     pace::oracle oracle(instance, ub);
 
-    std::vector<std::pair<T, T>> settled_pairs;
-    uint32_t c = 0, b = 0, p = 0;
-    const std::size_t n = graph.get_n_free();
+    std::vector<std::pair<T, T>> forced_pairs;
+    std::vector<std::pair<T, T>> more_settled_pairs;
+    uint32_t d = 0, c = 0, b = 0, p = 0;
     for (T u = 0; u < n; ++u) {
         for (T v = u + 1; v < n; ++v) {
+            pace::pattern pattern = oracle.based_on_degree(u, v);
+            if (pattern != pace::indeterminate) {
+                ++d;
+                insert_based_on_pattern(more_settled_pairs, restriction_graph, u, v, pattern);
+                continue;
+            }
+
             const R& c_uv = cr_matrix(u, v);
             const R& c_vu = cr_matrix(v, u);
             if (c_uv == c_vu) continue;
 
             // c_uv != c_vu from her
-            pace::pattern pattern = oracle.based_on_crossing_numbers(c_uv, c_vu);
+            pattern = oracle.based_on_crossing_numbers(c_uv, c_vu);
             if (pattern != pace::indeterminate) {
                 ++c;
-                insert_based_on_pattern(settled_pairs, digraph, u, v, pattern);
+                insert_based_on_pattern(forced_pairs, restriction_graph, u, v, pattern);
                 continue;
             }
 
             pattern = oracle.based_on_bounds(c_uv, c_vu);
             if (pattern != pace::indeterminate) {
                 ++b;
-                insert_based_on_pattern(settled_pairs, digraph, u, v, pattern);
+                insert_based_on_pattern(forced_pairs, restriction_graph, u, v, pattern);
                 continue;
             }
 
             pattern = oracle.based_on_pattern(u, v, c_uv, c_vu);
             if (pattern != pace::indeterminate) {
                 ++p;
-                insert_based_on_pattern(settled_pairs, digraph, u, v, pattern);
+                insert_based_on_pattern(forced_pairs, restriction_graph, u, v, pattern);
                 continue;
             }
         }
     }
 
     std::vector<std::pair<T, T>> new_arcs;
-    pace::transitive_hull(digraph, new_arcs);
+    pace::transitive_hull(restriction_graph, new_arcs);
     uint32_t h = new_arcs.size();
-    settled_pairs.insert(settled_pairs.end(), new_arcs.begin(), new_arcs.end());
 
     int32_t optimal = -1;
     if (do_test) {
@@ -80,28 +88,30 @@ void test_position_oracle(const pace::instance<T, R>& instance, const bool do_te
             instance.filepath, graph.get_n_fixed(), graph.get_n_free(), ordering);
         std::vector<T> positions;
         pace::inverse(ordering, positions);
-        for (const auto& [u, v] : settled_pairs) {
+        for (const auto& [u, v] : forced_pairs) {
             assert(positions[u] < positions[v]);
         }
         optimal = pace::test::get_ref_nof_crossings(instance.filepath);
     }
+    assert(pace::topological_sort(restriction_graph, ordering));
 
-    std::size_t n_choose_2 = n * (n - 1) / 2;
-    fmt::printf("%11s|%11u%11u%11.1f|%11u%11u%11u%11u|%11u%11d%11u\n",  //
+    const std::size_t n_choose_2 = n * (n - 1) / 2;
+    const uint32_t nof_settled = d + c + b + p + h;
+    fmt::printf("%11s|%11u%11u%11.1f|%11u%11u%11u%11u%11u|%11u%11d%11u\n",  //
                 instance.filepath.filename(),                           //
-                n_choose_2, settled_pairs.size(),
-                static_cast<double>(settled_pairs.size()) / n_choose_2 * 100,  //
-                c, b, p, h,                                                    //
+                n_choose_2, nof_settled,
+                static_cast<double>(nof_settled) / n_choose_2 * 100,  //
+                d, c, b, p, h,                                                   //
                 instance.get_lower_bound(), optimal, ub);
     std::cout << std::flush;
 }
 
 void test_pattern_analyzer_with(const fs::path dirpath, const bool do_test) {
     fmt::printf("start test_pattern_analyzer_with(%s)\n\n", dirpath);
-    fmt::printf("%11s|%11s%11s%11s|%11s%11s%11s%11s|%11s%11s%11s\n",
+    fmt::printf("%11s|%11s%11s%11s|%11s%11s%11s%11s%11s|%11s%11s%11s\n",
                 "instance",                                 //
                 "vars", "settled", "%%",                    //
-                "crossing n", "bounds", "pattern", "hull",  //
+                "degree", "crossing n", "bounds", "pattern", "hull",  //
                 "lb", "optimal", "ub");
     pace::test::print_line(125);
     for (const auto& file : fs::directory_iterator(dirpath)) {
