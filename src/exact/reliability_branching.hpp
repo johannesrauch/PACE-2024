1d6a290ec894d6a7a838fc7276113c791a453e24
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-17 22:06:00 +0200

    formatting

--- src/exact/reliability_branching.hpp
+++ src/exact/reliability_branching.hpp
@@ -20,8 +20,8 @@ class reliability_branching {
 
     std::vector<double> up_cost_sums;
     std::vector<double> down_cost_sums;
-    std::vector<crossing_number_t> n_up_summands;
-    std::vector<crossing_number_t> n_down_summands;
+    std::vector<uint32_t> n_up_samples;
+    std::vector<uint32_t> n_down_samples;
 
     std::vector<double> scores;
 
@@ -31,22 +31,24 @@ class reliability_branching {
           n_cols(lp.get_n_cols()),
           up_cost_sums(n_cols),
           down_cost_sums(n_cols),
-          n_up_summands(n_cols),
-          n_down_summands(n_cols),
+          n_up_samples(n_cols),
+          n_down_samples(n_cols),
           scores(n_cols) {}
 
     void update_up_cost(const std::size_t j, const double obj_val_new, const double obj_val_old, const double x_j) {
+        assert(j < n_cols);
         assert(obj_val_new >= obj_val_old);
-        assert(0. < x_j && x_j < 1.);
+        assert(!lp.is_column_integral(j));
         up_cost_sums[j] += (obj_val_new - obj_val_old) / (1. - x_j);
-        ++n_up_summands[j];
+        ++n_up_samples[j];
     }
 
     void update_down_cost(const std::size_t j, const double obj_val_new, const double obj_val_old, const double x_j) {
+        assert(j < n_cols);
         assert(obj_val_new >= obj_val_old);
-        assert(0. < x_j && x_j < 1.);
+        assert(!lp.is_column_integral(j));
         down_cost_sums[j] += (obj_val_new - obj_val_old) / x_j;
-        ++n_down_summands[j];
+        ++n_down_samples[j];
     }
 
     inline double score(const double &up_cost, const double &down_cost) {
@@ -57,36 +59,38 @@ class reliability_branching {
         // scores of uninitialized columns is the average, which we compute first
         double up_cost_avg = 0.;
         double down_cost_avg = 0.;
-        crossing_number_t up_cost_summands = 0;
-        crossing_number_t down_cost_summands = 0;
+        crossing_number_t n_up_cost_summands = 0;
+        crossing_number_t n_down_cost_summands = 0;
+
         for (std::size_t j = 0; j < n_cols; ++j) {
-            if (n_up_summands[j] > 0) {
+            if (n_up_samples[j] > 0) {
                 up_cost_avg += up_cost_sums[j];
-                up_cost_summands += n_up_summands[j];
+                n_up_cost_summands += n_up_samples[j];
             }
-            if (n_down_summands[j] > 0) {
+            if (n_down_samples[j] > 0) {
                 down_cost_avg += down_cost_sums[j];
-                down_cost_summands += n_down_summands[j];
+                n_down_cost_summands += n_down_samples[j];
             }
         }
-        if (up_cost_summands > 0) {
-            up_cost_avg /= up_cost_summands;
+        
+        if (n_up_cost_summands > 0) {
+            up_cost_avg /= n_up_cost_summands;
         }
-        if (down_cost_summands > 0) {
-            down_cost_avg /= down_cost_summands;
+        if (n_down_cost_summands > 0) {
+            down_cost_avg /= n_down_cost_summands;
         }
 
         // store scores
         for (std::size_t j = 0; j < n_cols; ++j) {
             const double x_j = col_value[j];
             double up_cost =             //
-                (n_up_summands[j] > 0  //
-                     ? up_cost_sums[j] / n_up_summands[j]
+                (n_up_samples[j] > 0  //
+                     ? up_cost_sums[j] / n_up_samples[j]
                      : up_cost_avg)  //
                 * (1. - x_j);
             double down_cost =             //
-                (n_down_summands[j] > 0  //
-                     ? down_cost_sums[j] / n_down_summands[j]
+                (n_down_samples[j] > 0  //
+                     ? down_cost_sums[j] / n_down_samples[j]
                      : down_cost_avg)  //
                 * x_j;
             scores[j] = score(up_cost, down_cost);
@@ -136,17 +140,17 @@ class reliability_branching {
         // do strong branching for unreliable pseudo costs
         std::size_t best = *candidates.rbegin();
         double best_score = scores[best];
-        std::size_t it_since_improve = 0;
+        std::size_t n_it_since_improve = 0;
         for (const std::size_t &j : candidates) {
-            if (std::min(n_up_summands[j], n_down_summands[j]) < PACE_CONST_RELIBRANCH_RELIPARAM) {
+            if (std::min(n_up_samples[j], n_down_samples[j]) < PACE_CONST_RELIBRANCH_RELIPARAM) {
                 scores[j] = kinda_strong_branching(j);
                 if (scores[j] > best_score) {
                     best = j;
                     best_score = scores[j];
-                    it_since_improve = 0;
+                    n_it_since_improve = 0;
                 } else {
-                    ++it_since_improve;
-                    if (it_since_improve >= PACE_CONST_RELIBRANCH_LOOKAHEAD) break;
+                    ++n_it_since_improve;
+                    if (n_it_since_improve >= PACE_CONST_RELIBRANCH_LOOKAHEAD) break;
                 }
             }
         }
@@ -154,19 +158,19 @@ class reliability_branching {
         return best;
     }
 
-    double get_column_value(const std::size_t j) const {
+    inline double get_column_value(const std::size_t j) const {
         assert(j < n_cols);
         return col_value[j];
     }
 
-    double get_up_cost(const std::size_t j) const {
+    inline double get_up_cost(const std::size_t j) const {
         assert(j < n_cols);
-        return up_cost_sums[j] / n_up_summands[j];
+        return up_cost_sums[j] / n_up_samples[j];
     }
 
-    double get_down_cost(const std::size_t j) const {
+    inline double get_down_cost(const std::size_t j) const {
         assert(j < n_cols);
-        return down_cost_sums[j] / n_down_summands[j];
+        return down_cost_sums[j] / n_down_samples[j];
     }
 };
 
