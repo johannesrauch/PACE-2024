3f19e968b12f644368b78f3c1df91f805a576ced
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-23 12:10:21 +0200

    fixed namespace issues

--- src/weberknecht.cpp
+++ src/weberknecht.cpp
@@ -2,9 +2,9 @@
 #include "io/output.hpp"
 
 int main() {
-    input in;
-    exact_solver solver(in);
-    std::vector<vertex_t> ordering;
+    pace::input in;
+    pace::exact_solver solver(in);
+    std::vector<pace::vertex_t> ordering;
     solver(ordering);
-    print_output(in.get_graph().get_n_fixed(), ordering);
+    pace::print_output(in.get_graph().get_n_fixed(), ordering);
 }
