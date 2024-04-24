d40bcb62a40987b064ccbf40a9997f726f301de1
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-24 20:02:11 +0200

    weberknecht main

--- src/weberknecht.cpp
+++ src/weberknecht.cpp
@@ -1,10 +1,24 @@
+#include <fstream>
+
 #include "exact/exact_solver.hpp"
 #include "io/output.hpp"
 
-int main() {
-    pace::input in;
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        pace::print_help();
+        return 1;
+    }
+
+    pace::input in(argv[1]);
     pace::exact_solver solver(in);
     std::vector<pace::vertex_t> ordering;
     solver(ordering);
-    pace::print_output(in.get_graph().get_n_fixed(), ordering);
+
+    if (argc < 3) {
+        pace::print_output(in.get_graph().get_n_fixed(), ordering);
+    } else {
+        std::ofstream out_f(argv[2]);
+        pace::print_output(out_f, in.get_graph().get_n_fixed(), ordering);
+    }
+    return 0;
 }
