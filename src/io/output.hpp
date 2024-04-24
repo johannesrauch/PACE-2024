54e7dc13aede8682295187f0d265a821b14dfb00
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-24 19:38:28 +0200

    print output, ostream param

--- src/io/output.hpp
+++ src/io/output.hpp
@@ -1,16 +1,22 @@
 #ifndef PACE_IO_OUTPUT_HPP
 #define PACE_IO_OUTPUT_HPP
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
 namespace pace {
 
-template <typename T>
-void print_output(const std::size_t n_fixed, const std::vector<T>& ordering) {
-    for (const T& element: ordering) {
-        std::cout << element + n_fixed + 1 << std::endl;
+template <typename CharT, typename Traits, typename T, typename Allocator>
+void print_output(  //
+    std::basic_istream<CharT, Traits> ostream, const std::size_t n_fixed, const std::vector<T, Allocator>& ordering) {
+    for (const T& element : ordering) {
+        ostream << element + n_fixed + 1 << std::endl;
     }
+}
+
+template <typename T, typename Allocator>
+void print_output(const std::size_t n_fixed, const std::vector<T, Allocator>& ordering) {
+    print_output(std::cout, n_fixed, ordering);
 }
 
 };  // namespace pace
