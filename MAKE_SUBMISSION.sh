b1880c1fbfb12d8da5f9c957592201d374433d9a
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-24 20:24:48 +0200

    not needed

--- MAKE_SUBMISSION.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf highs/bin highs/build highs/include highs/lib
-cd highs
-mkdir build
-cd build
-cmake ..
-rm -rf _deps/highs-src/.git
-cd ../..
-tar -cvz -f sub.tar.gz highs/build src CMakeLists.txt
