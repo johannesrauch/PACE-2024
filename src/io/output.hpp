2b3cf9d28f638704fec2402a1ac43364927eb26f
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-24 19:59:15 +0200

    must be ref param

--- src/io/output.hpp
+++ src/io/output.hpp
@@ -6,13 +6,11 @@
 
 namespace pace {
 
-void print_help() {
-    std::cout << "Usage: $ weberknecht <path/to/input.gr> <path/to/output.sol>" << std::endl;
-}
+void print_help() { std::cout << "Usage: $ weberknecht <path/to/input.gr> <path/to/output.sol>" << std::endl; }
 
 template <typename CharT, typename Traits, typename T, typename Allocator>
 void print_output(  //
-    std::basic_ostream<CharT, Traits> ostream, const std::size_t n_fixed, const std::vector<T, Allocator>& ordering) {
+    std::basic_ostream<CharT, Traits>& ostream, const std::size_t n_fixed, const std::vector<T, Allocator>& ordering) {
     for (const T& element : ordering) {
         ostream << element + n_fixed + 1 << std::endl;
     }
