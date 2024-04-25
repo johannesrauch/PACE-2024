5d2963b9de84b7dcdf4b5ac9c57f516db8873e27
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-25 17:13:59 +0200

    template for topsort, for heuristic we dont want to return prematurely

--- src/utils/topological_sort.hpp
+++ src/utils/topological_sort.hpp
@@ -16,6 +16,7 @@ namespace internal {
  * used by topological_sort below.
  *
  * @tparam T vertex type
+ * @tparam A allocator
  * @param graph
  * @param ordering vector, where is stored
  * @param visited
@@ -23,24 +24,26 @@ namespace internal {
  * @return true if a topological sort was found
  * @return false if a (directed) cycle was found
  */
-template <typename T>
+template <bool RETURN_IF_CYCLIC = true, typename T, typename A>
 bool topological_sort_dfs(const general_digraph<T>& graph,
-                          std::vector<T>& ordering,       //
+                          std::vector<T, A>& ordering,       //
                           std::vector<uint8_t>& visited,  //
                           T v) {
     visited[v] = 1;
     auto& neighbors = graph.get_neighbors(v);
+    bool acyclic = true;
     for (const T& u : neighbors) {
         if (visited[u] == 1 ||
             (visited[u] == 0 &&
              !topological_sort_dfs(graph, ordering, visited, u))) {
             // cycle found
-            return false;
+            acyclic = false;
+            if constexpr(RETURN_IF_CYCLIC) return acyclic;
         }
     }
     visited[v] = 2;
     ordering.emplace_back(v);
-    return true;
+    return acyclic;
 }
 
 };
@@ -50,29 +53,32 @@ bool topological_sort_dfs(const general_digraph<T>& graph,
  * and stores it in the vector ordering.
  *
  * @tparam T vertex type
+ * @tparam A allocator
  * @param graph
  * @param ordering
  * @return true if a topological sort was found
  * @return false if a (directed) cycle was found
  */
-template <typename T>
-bool topological_sort(const general_digraph<T>& graph, std::vector<T>& ordering) {
+template <bool RETURN_IF_CYCLIC = true, typename T, typename A>
+bool topological_sort(const general_digraph<T>& graph, std::vector<T, A>& ordering) {
     const std::size_t n = graph.get_n();
     ordering.clear();
     ordering.reserve(n);
     std::vector<uint8_t> visited(n, 0);
 
+    bool acyclic = true;
     for (T v = 0; v < n; ++v) {
         if (visited[v] == 0 &&
-            !internal::topological_sort_dfs(graph, ordering, visited, v)) {
+            !internal::topological_sort_dfs<RETURN_IF_CYCLIC, T, A>(graph, ordering, visited, v)) {
             // cycle found
-            return false;
+            acyclic = false;
+            if constexpr(RETURN_IF_CYCLIC) return acyclic;
         }
     }
 
     std::reverse(ordering.begin(), ordering.end());
     assert(graph.get_n() == ordering.size());
-    return true;
+    return acyclic;
 }
 
 };  // namespace pace
