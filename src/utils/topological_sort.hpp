b0f02faa5b162fe96f6af18626d4b518bf1fbb0a
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-26 09:30:42 +0200

    topsort for relax_heuristic

--- src/utils/topological_sort.hpp
+++ src/utils/topological_sort.hpp
@@ -6,6 +6,7 @@
 #include <stack>
 
 #include "model/digraph.hpp"
+#include "utils/randomness_utils.hpp"
 
 namespace pace {
 
@@ -21,32 +22,31 @@ namespace internal {
  * @param ordering vector, where is stored
  * @param visited
  * @param v current vertex
- * @return true if a topological sort was found
- * @return false if a (directed) cycle was found
+ * @return number of cycles found
  */
 template <bool RETURN_IF_CYCLIC = true, typename T, typename A>
-bool topological_sort_dfs(const general_digraph<T>& graph,
-                          std::vector<T, A>& ordering,       //
-                          std::vector<uint8_t>& visited,  //
-                          T v) {
+std::size_t topological_sort_dfs(const general_digraph<T>& graph, std::vector<T, A>& ordering,  //
+                                 std::vector<uint8_t>& visited,                                 //
+                                 T v) {
     visited[v] = 1;
     auto& neighbors = graph.get_neighbors(v);
-    bool acyclic = true;
+    std::size_t n_cycles = 0;
     for (const T& u : neighbors) {
-        if (visited[u] == 1 ||
-            (visited[u] == 0 &&
-             !topological_sort_dfs(graph, ordering, visited, u))) {
-            // cycle found
-            acyclic = false;
-            if constexpr(RETURN_IF_CYCLIC) return acyclic;
-        }
+        std::size_t n_cycles_ = 0;
+        if (visited[u] == 0)
+            n_cycles_ = topological_sort_dfs(graph, ordering, visited, u);
+        else if (visited[u] == 1)
+            n_cycles_ = 1;
+        n_cycles += n_cycles_;
+        if constexpr (RETURN_IF_CYCLIC)
+            if (n_cycles_ > 0) return 1;
     }
     visited[v] = 2;
     ordering.emplace_back(v);
-    return acyclic;
+    return n_cycles;
 }
 
-};
+};  // namespace internal
 
 /**
  * @brief computes a topological sort (if possible) of the given graph
@@ -59,26 +59,54 @@ bool topological_sort_dfs(const general_digraph<T>& graph,
  * @return true if a topological sort was found
  * @return false if a (directed) cycle was found
  */
-template <bool RETURN_IF_CYCLIC = true, typename T, typename A>
+template <typename T, typename A>
 bool topological_sort(const general_digraph<T>& graph, std::vector<T, A>& ordering) {
     const std::size_t n = graph.get_n();
     ordering.clear();
     ordering.reserve(n);
     std::vector<uint8_t> visited(n, 0);
 
-    bool acyclic = true;
     for (T v = 0; v < n; ++v) {
-        if (visited[v] == 0 &&
-            !internal::topological_sort_dfs<RETURN_IF_CYCLIC, T, A>(graph, ordering, visited, v)) {
+        if (visited[v] == 0 && internal::topological_sort_dfs(graph, ordering, visited, v) > 0) {
             // cycle found
-            acyclic = false;
-            if constexpr(RETURN_IF_CYCLIC) return acyclic;
+            return false;
         }
     }
 
     std::reverse(ordering.begin(), ordering.end());
     assert(graph.get_n() == ordering.size());
-    return acyclic;
+    return true;
+}
+
+/**
+ * @brief computes a topological sort (if possible) of the given graph
+ * and stores it in the vector ordering.
+ *
+ * @tparam T vertex type
+ * @tparam A allocator
+ * @param graph
+ * @param ordering
+ * @return number of cycles found
+ */
+template <typename T, typename A>
+std::size_t topological_sort_rd(const general_digraph<T>& graph, std::vector<T, A>& ordering) {
+    const std::size_t n = graph.get_n();
+    ordering.clear();
+    ordering.reserve(n);
+    std::vector<uint8_t> visited(n, 0);
+    std::vector<T> vertices(n);
+    pace::test::shuffle(vertices);  // if graph has cycle (see relax_heuristic), this may be meaningful
+
+    std::size_t n_cycles = 0;
+    for (const T& v : vertices) {
+        if (visited[v] == 0) {
+            n_cycles += internal::topological_sort_dfs<false, T, A>(graph, ordering, visited, v);
+        }
+    }
+
+    std::reverse(ordering.begin(), ordering.end());
+    assert(graph.get_n() == ordering.size());
+    return n_cycles;
 }
 
 };  // namespace pace
