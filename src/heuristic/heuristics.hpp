416be0a435415c64a8810d0e7e5390a4249a427e
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-30 15:03:13 +0200

    ctor calls

--- src/heuristic/heuristics.hpp
+++ src/heuristic/heuristics.hpp
@@ -12,10 +12,17 @@ class heuristics : public instance_view {
     std::vector<vertex_t> another_ordering;
     barycenter_heuristic barycenter_h;
     probmedian_heuristic probmedian_h;
+    round_heuristic round_h;
+    sort_heuristic sort_h;
 
    public:
     heuristics(instance &instance_)
-        : instance_view(instance_), another_ordering(n_free), barycenter_h(instance_), probmedian_h(instance_) {}
+        : instance_view(instance_),
+          another_ordering(n_free),
+          barycenter_h(instance_),
+          probmedian_h(instance_),
+          round_h(instance_),
+          sort_h(instance_) {}
 
     crossing_number_t uninformed(std::vector<vertex_t> &ordering) {
         PACE_DEBUG_PRINTF("start uninformed heuristics\n");
@@ -29,6 +36,21 @@ class heuristics : public instance_view {
         PACE_DEBUG_PRINTF("end   uninformed heuristics\n");
         return n_cr;
     }
+
+    crossing_number_t informed(highs_lp &lp, std::vector<vertex_t> &ordering) {
+        PACE_DEBUG_PRINTF("start informed heuristics\n");
+        crossing_number_t n_cr = sort_h(lp, ordering);
+        if (n_cr <= lower_bound()) return n_cr;
+        const crossing_number_t n_cr_r = round_h(lp, another_ordering);
+        if (n_cr_r < n_cr) {
+            n_cr = n_cr_r;
+            std::swap(another_ordering, ordering);
+        }
+        PACE_DEBUG_PRINTF("end   informed heuristics, ub=%s\n", upper_bound);
+        return n_cr;
+    }
+
+    double get_confidence() const { return round_h.get_confidence(); }
 };
 
 };  // namespace pace
