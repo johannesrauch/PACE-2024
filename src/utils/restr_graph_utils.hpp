d03e617dc354fc59454d6cc416e97d4c682b1c41
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-04-30 12:51:41 +0200

    moved function from internal

--- src/utils/restr_graph_utils.hpp
+++ src/utils/restr_graph_utils.hpp
@@ -13,7 +13,7 @@ namespace internal {
 /**
  * @brief see https://math.stackexchange.com/a/4166138.
  */
-inline double smooth_transition(const double x, const double k = 2.5) {
+inline double smooth_transition(const double x, const double k = 2.) {
     assert(x > 1e-6);
     assert(x < 1. - 1e-6);
     const double y = 0.5 * (std::tanh(k * (2 * x - 1) / (2 * std::sqrt(x * (1 - x)))) + 1);
@@ -21,6 +21,8 @@ inline double smooth_transition(const double x, const double k = 2.5) {
     assert(y <= 1);
     return y;
 }
+
+};  // namespace internal
 
 template <typename T>
 void build_restr_graph(const std::vector<double> &column_values,             //
@@ -31,7 +33,7 @@ void build_restr_graph(const std::vector<double> &column_values,             //
     std::size_t i = 0;
     for (const auto &[u, v] : unsettled_pairs) {
         const double x = column_values[i];
-        const bool add_uv = x > 1e-6 && (x >= 1. - 1e-6 || coinflip(smooth_transition(x)));
+        const bool add_uv = x > 1e-6 && (x >= 1. - 1e-6 || coinflip(internal::smooth_transition(x)));
         if (add_uv)
             restr_graph.add_arc(u, v);
         else
@@ -40,14 +42,12 @@ void build_restr_graph(const std::vector<double> &column_values,             //
     }
 }
 
-};  // namespace internal
-
 template <typename T>
 bool build_restr_graph_ordering(const std::vector<double> &column_values,             //
                                 const std::vector<std::pair<T, T>> &unsettled_pairs,  //
                                 general_digraph<T> &restr_graph,                      //
                                 std::vector<T> &ordering) {
-    internal::build_restr_graph(column_values, unsettled_pairs, restr_graph);
+    build_restr_graph(column_values, unsettled_pairs, restr_graph);
     return topological_sort(restr_graph, ordering);
 }
 
