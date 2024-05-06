0f48936ba1c5fb555cc160bfe374f7ef0b0aebb6
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-05-06 10:19:38 +0200

    per stdio

--- src/weberknecht.cpp
+++ src/weberknecht.cpp
@@ -4,21 +4,10 @@
 #include "io/output.hpp"
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        pace::print_help();
-        return 1;
-    }
-
-    pace::input in(argv[1]);
+    pace::input in;
     pace::exact_solver solver(in);
     std::vector<pace::vertex_t> ordering;
     solver(ordering);
-
-    if (argc < 3) {
-        pace::print_output(in.get_graph().get_n_fixed(), ordering);
-    } else {
-        std::ofstream out_f(argv[2]);
-        pace::print_output(out_f, in.get_graph().get_n_fixed(), ordering);
-    }
+    pace::print_output(in.get_graph().get_n_fixed(), ordering);
     return 0;
 }
