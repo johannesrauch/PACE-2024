3e6a0f97607f58fe60051d6d09b599cae547a273
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-05-16 13:00:11 +0200

    highs_base.cpp

---
+++ src/exact/highs_base.cpp
@@ -0,0 +1,85 @@
+#include "exact/highs_base.hpp"
+
+namespace pace {
+
+//
+// ctor
+//
+
+highs_base::highs_base(instance &instance_) : instance_view(instance_) {
+    status = solver.setOptionValue("presolve", "off");
+    assert(status == HighsStatus::kOk);
+    status = solver.setOptionValue("solver", "simplex");
+    assert(status == HighsStatus::kOk);
+    status = solver.setOptionValue("parallel", "off");
+    assert(status == HighsStatus::kOk);
+    status = solver.setOptionValue("log_to_console", false);
+    assert(status == HighsStatus::kOk);
+
+    PACE_DEBUG_PRINTF("start add_columns\n");
+    add_columns();
+    PACE_DEBUG_PRINTF("end   add_columns\n");
+}
+
+//
+// column methods
+//
+
+void highs_base::add_columns() {
+    crossing_number_t obj_offset = objective_offset();
+
+    // add variables which are not yet settled
+    const crossing_matrix &cr{cr_matrix()};
+    for (const auto &[u, v] : unsettled_pairs()) {
+        const HighsInt l = solver.getNumCol();
+        assert(l == magic()[flat_index(n_free, n_free_2, u, v)]);
+        add_column();
+
+        const crossing_number_t &c_uv = cr(u, v);
+        const crossing_number_t &c_vu = cr(v, u);
+        obj_offset += c_vu;
+        if (c_uv != c_vu) {
+            change_column_cost(
+                l, static_cast<double>(c_uv) - static_cast<double>(c_vu));
+        }
+    }
+
+    // set constant term (shift/offset) in the objective function
+    solver.changeObjectiveOffset(obj_offset);
+    assert(get_n_cols() <= n_free_2);
+}
+
+//
+// row methods
+//
+
+void highs_base::add_3cycle_row_to_aux_vectors(const vertex_t &u,
+                                               const vertex_t &v,
+                                               const vertex_t &w) {
+    // at least two must be in the lp as variables since we compute transitive
+    // hull in oracle
+    assert(u < v);
+    assert(v < w);
+
+    const auto [uv, vw, uw] = flat_indices(n_free, n_free_2, u, v, w);
+    const double bound_offset = get_3cycle_bound_offset(uv, vw, uw);
+    const std::vector<magic_t> &m = magic();
+
+    lower_bounds.emplace_back(0. + bound_offset);
+    upper_bounds.emplace_back(1. + bound_offset);
+    starts.emplace_back(indices.size());
+    if (m[uv] >= 0) {
+        indices.emplace_back(m[uv]);
+        values.emplace_back(1.);
+    }
+    if (m[uw] >= 0) {
+        indices.emplace_back(m[uw]);
+        values.emplace_back(-1.);
+    }
+    if (m[vw] >= 0) {
+        indices.emplace_back(m[vw]);
+        values.emplace_back(1.);
+    }
+}
+
+};  // namespace pace
--- src/exact/highs_base.hpp
+++ src/exact/highs_base.hpp
@@ -20,51 +20,18 @@ class highs_base : public instance_view {
     std::vector<HighsInt> indices;
     std::vector<double> values;
 
-    highs_base(instance &instance_) : instance_view(instance_) {
-        status = solver.setOptionValue("presolve", "off");
-        assert(status == HighsStatus::kOk);
-        status = solver.setOptionValue("solver", "simplex");
-        assert(status == HighsStatus::kOk);
-        status = solver.setOptionValue("parallel", "off");
-        assert(status == HighsStatus::kOk);
-        status = solver.setOptionValue("log_to_console", false);
-        assert(status == HighsStatus::kOk);
-
-        PACE_DEBUG_PRINTF("start add_columns\n");
-        add_columns();
-        PACE_DEBUG_PRINTF("end   add_columns\n");
-    }
+    highs_base(instance &instance_);
 
     /*
      * column methods
      */
 
     /**
-     * @brief adds all variables of the ilp formulation of one-sided crossing minimization
-     * and sets their objective coefficients from the crossing numbers
+     * @brief adds all variables of the ilp formulation of one-sided crossing
+     * minimization and sets their objective coefficients from the crossing
+     * numbers
      */
-    inline void add_columns() {
-        crossing_number_t obj_offset = objective_offset();
-
-        // add variables which are not yet settled
-        const crossing_matrix &cr{cr_matrix()};
-        for (const auto &[u, v] : unsettled_pairs()) {
-            const HighsInt l = solver.getNumCol();
-            assert(l == magic()[flat_index(n_free, n_free_2, u, v)]);
-            add_column();
-
-            const crossing_number_t &c_uv = cr(u, v);
-            const crossing_number_t &c_vu = cr(v, u);
-            obj_offset += c_vu;
-            if (c_uv != c_vu) {
-                change_column_cost(l, static_cast<double>(c_uv) - static_cast<double>(c_vu));
-            }
-        }
-
-        // set constant term (shift/offset) in the objective function
-        solver.changeObjectiveOffset(obj_offset);
-        assert(get_n_cols() <= n_free_2);
-    }
+    void add_columns();
 
     /**
      * @brief adds a column with bounds 0. <= (*) <= 1.
@@ -88,42 +55,24 @@ class highs_base : public instance_view {
      */
 
     /**
-     * @brief prepares and adds entries to auxiliary vectors for adding 3-cycle ieq uvw/uwv to lp
+     * @brief prepares and adds entries to auxiliary vectors for adding 3-cycle
+     * ieq uvw/uwv to lp
      */
-    inline void add_3cycle_row_to_aux_vectors(const vertex_t &u, const vertex_t &v, const vertex_t &w) {
-        // at least two must be in the lp as variables since we compute transitive hull in oracle
-        assert(u < v);
-        assert(v < w);
-
-        const auto [uv, vw, uw] = flat_indices(n_free, n_free_2, u, v, w);
-        const double bound_offset = get_3cycle_bound_offset(uv, vw, uw);
-        const std::vector<magic_t> &m = magic();
-
-        lower_bounds.emplace_back(0. + bound_offset);
-        upper_bounds.emplace_back(1. + bound_offset);
-        starts.emplace_back(indices.size());
-        if (m[uv] >= 0) {
-            indices.emplace_back(m[uv]);
-            values.emplace_back(1.);
-        }
-        if (m[uw] >= 0) {
-            indices.emplace_back(m[uw]);
-            values.emplace_back(-1.);
-        }
-        if (m[vw] >= 0) {
-            indices.emplace_back(m[vw]);
-            values.emplace_back(1.);
-        }
-    }
+    void add_3cycle_row_to_aux_vectors(  //
+        const vertex_t &u, const vertex_t &v, const vertex_t &w);
 
     /*
      * getter
      */
 
    public:
-    void copy_column_values(std::vector<double> &col_value) const { col_value = solver.getSolution().col_value; }
+    void copy_column_values(std::vector<double> &col_value) const {
+        col_value = solver.getSolution().col_value;
+    }
 
-    const std::vector<double> &get_column_values() { return solver.getSolution().col_value; }
+    const std::vector<double> &get_column_values() {
+        return solver.getSolution().col_value;
+    }
 
     /**
      * @brief returns value of column j
@@ -159,7 +108,9 @@ class highs_base : public instance_view {
     /**
      * @brief returns the objective value of the lp
      */
-    double get_objective_value() const { return solver.getInfo().objective_function_value; }
+    double get_objective_value() const {
+        return solver.getInfo().objective_function_value;
+    }
 
     /**
      * @brief returns the objective value of the lp rounded to the next integer
@@ -179,11 +130,13 @@ class highs_base : public instance_view {
     }
 
     /**
-     * @brief returns the lower and upper bound offset for 3-cycle inequalities ijk
-     * due to permanently fixed variables that are not incorporated in the lp
+     * @brief returns the lower and upper bound offset for 3-cycle inequalities
+     * ijk due to permanently fixed variables that are not incorporated in the
+     * lp
      */
     inline int  //
-    get_3cycle_bound_offset(const std::size_t uv, const std::size_t vw, const std::size_t uw) {
+    get_3cycle_bound_offset(const std::size_t uv, const std::size_t vw,
+                            const std::size_t uw) {
         assert(uv < n_free_2);
         assert(vw < n_free_2);
         assert(uw < n_free_2);
@@ -196,8 +149,9 @@ class highs_base : public instance_view {
     }
 
     /**
-     * @brief returns the lower and upper bound offset for 3-cycle inequality with index i
-     * due to permanently fixed variables that are not incorporated in the lp
+     * @brief returns the lower and upper bound offset for 3-cycle inequality
+     * with index i due to permanently fixed variables that are not incorporated
+     * in the lp
      */
     inline std::pair<double, double> get_row_bounds(const std::size_t i) {
         assert(i < get_n_rows());
@@ -218,7 +172,8 @@ class highs_base : public instance_view {
     /**
      * @brief returns number of variables x_uv, x_vw, x_uw are in the lp
      */
-    inline int8_t get_n_vars_in_lp(const vertex_t &u, const vertex_t &v, const vertex_t &w) {
+    inline int8_t get_n_vars_in_lp(const vertex_t &u, const vertex_t &v,
+                                   const vertex_t &w) {
         const auto [uv, vw, uw] = flat_indices(n_free, n_free_2, u, v, w);
         uint8_t n_vars_in_lp{0};
         const std::vector<magic_t> m{magic()};
