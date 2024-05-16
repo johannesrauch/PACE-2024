ebcc1f0a2f9dcf11635fb10a6ed332bec7c02de9
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-05-16 17:54:47 +0200

    debug print

--- src/heuristic/rins_heuristic.cpp
+++ src/heuristic/rins_heuristic.cpp
@@ -6,11 +6,13 @@ namespace pace {
 
 crossing_number_t rins_heuristic::operator()(  //
     highs_lp &lp, std::vector<vertex_t> &ordering) {
+    PACE_DEBUG_PRINTF("start rins\n");
     std::unique_ptr<instance> subinstance_ptr(instance_.new_rins_instance(lp));
     branch_and_cut_params params;
     params.max_nodes = 100;
     branch_and_cut solver(*subinstance_ptr, params);
     const crossing_number_t n_cr = solver(ordering);
+    PACE_DEBUG_PRINTF("end   rins\n");
     return shift_h(ordering, n_cr);
 }
 
