68d9a5d24135419b4953c3d231be4b8864b8091a
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-05-17 20:07:03 +0200

    heuristic

--- run.py
+++ run.py
@@ -16,15 +16,17 @@ if __name__ == "__main__":
     instances = os.listdir(instdir)
     instances.sort(key=lambda k: int(k.split(".")[0]))
     for filename in instances:
-        print(f"running '{weberk} < {instdir + filename}'")
+        print(f"running '{weberk} < {instdir + filename}'. ", end="")
         n += 1
         with open(instdir + filename) as i, open(outpdir + filename[:-2] + "log", "w") as o:
             try:
                 r = subprocess.call([weberk], stdin=i, stdout=o, timeout=timeout)
                 if r != 0: raise ValueError
                 succeded.append(filename)
+                print("success")
             except (subprocess.TimeoutExpired, ValueError):
                 failed.append(filename)
+                print("failure")
     
     output = f"number of instances: {n}\n"
     output += f"succeded {len(succeded) / n * 100 : 3.1f}%:\n{succeded}\n"
