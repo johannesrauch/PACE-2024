edbea173ce22a85bfd61c98331fcc7e6e657bbaa
Johannes Rauch <johannes.rauch@uni-ulm.de>
2024-05-21 07:57:29 +0200

    sort_heuristic.cpp

---
+++ src/heuristic/sort_heuristic.cpp
@@ -0,0 +1,24 @@
+#include "heuristic/sort_heuristic.hpp"
+
+namespace pace {
+
+crossing_number_t sort_heuristic::operator()(  //
+    highs_lp &lp, std::vector<vertex_t> &ordering) {
+    std::vector<double> precedence(n_free);
+    for (vertex_t u = 0; u < n_free; ++u) {
+        for (vertex_t v = 0; v < u; ++v)
+            precedence[u] +=
+                lp.get_variable_value(flat_index(n_free, n_free_2, v, u));
+        for (vertex_t v = u + 1u; v < n_free; ++v)
+            precedence[u] +=
+                1. - lp.get_variable_value(flat_index(n_free, n_free_2, u, v));
+    }
+    identity(n_free, ordering);
+    std::sort(ordering.begin(), ordering.end(),
+              [&](const vertex_t &u, const vertex_t &v) -> bool {
+                  return precedence[u] < precedence[v];
+              });
+    return shift_h(ordering);
+}
+
+}  // namespace pace
--- src/heuristic/sort_heuristic.hpp
+++ src/heuristic/sort_heuristic.hpp
@@ -11,26 +11,15 @@ class sort_heuristic : public instance_view {
     shift_heuristic shift_h;
 
    public:
-    sort_heuristic(instance &instance_) : instance_view(instance_), shift_h(instance_) {}
+    sort_heuristic(instance &instance_)
+        : instance_view(instance_), shift_h(instance_) {}
 
     sort_heuristic(const sort_heuristic &other) = delete;
     sort_heuristic(sort_heuristic &&other) = delete;
     sort_heuristic &operator=(const sort_heuristic &other) = delete;
     sort_heuristic &operator=(sort_heuristic &&other) = delete;
 
-    crossing_number_t operator()(highs_lp &lp, std::vector<vertex_t> &ordering) {
-        std::vector<double> precedence(n_free);
-        for (vertex_t u = 0; u < n_free; ++u) {
-            for (vertex_t v = 0; v < u; ++v)
-                precedence[u] += lp.get_variable_value(flat_index(n_free, n_free_2, v, u));
-            for (vertex_t v = u + 1u; v < n_free; ++v)
-                precedence[u] += 1. - lp.get_variable_value(flat_index(n_free, n_free_2, u, v));
-        }
-        identity(n_free, ordering);
-        std::sort(ordering.begin(), ordering.end(),
-                  [&](const vertex_t &u, const vertex_t &v) -> bool { return precedence[u] < precedence[v]; });
-        return shift_h(ordering);
-    }
+    crossing_number_t operator()(highs_lp &lp, std::vector<vertex_t> &ordering);
 };
 
 };  // namespace pace
